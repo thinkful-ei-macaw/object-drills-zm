8c750935b31633b6327437aaeba39c9f34cbc340
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-11 15:39:20 -0500

    added iteration over obj properties

--- drills-3.js
+++ drills-3.js
@@ -1,4 +1,5 @@
 'use strict';
+// problem 1 =========================
 const loaf = {
   flour: 300,
   water: 210,
@@ -9,3 +10,19 @@ const loaf = {
 
 console.log(loaf.flour, loaf.water);
 console.log(loaf.hydration());
+
+
+// problem 2 =========================
+let obj = {
+  foo: 'hi',
+  bar: 'my',
+  fum: 'name',
+  quux: 'is',
+  spam: 'greg'
+}
+
+for (const key in obj) {
+  if (obj.hasOwnProperty(key)) {
+    console.log(`${key}: '${obj[key]}'`);
+  }
+}
