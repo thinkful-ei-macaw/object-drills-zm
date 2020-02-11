dd5331e11492149c6dc68c91f11ad6f13d149bc0
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-11 16:53:31 -0500

    Added database search

--- drills-3.js
+++ drills-3.js
@@ -101,3 +101,28 @@ let hobbits = characters.filter(character => character.race === 'Hobbit');
 
 let baddies = characters.filter(character => character.attack > 5);
 
+
+// problem 8 =========================
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query){
+  let result = null;
+  result = arr.find(element => {
+    let match = true;
+    Object.keys(query).forEach(key => {
+      match = element[key] === query[key];
+    });
+    return match;
+  });
+  return result;
+}
+
+console.log( findOne(HEROES, { squad: 'Justice League' }) );
