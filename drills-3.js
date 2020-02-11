b2486e122a77342a6ad5e62fbe86a1cc54e4d32e
Zac Barreca <zacbarreca@gmail.com>
2020-02-11 17:06:43 -0500

    add drill 8a II

--- drills-3.js
+++ drills-3.js
@@ -126,3 +126,30 @@ function findOne(arr, query){
 }
 
 console.log( findOne(HEROES, { squad: 'Justice League' }) );
+
+// problem 8A II =========================
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne: function(query){
+    let result = null;
+    result = this.store.heroes.find(element => {
+      let match = true;
+      Object.keys(query).forEach(key => {
+        match = element[key] === query[key];
+      });
+      return match;
+    });
+    return result;
+  }
+};
