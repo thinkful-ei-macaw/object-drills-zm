13c500249b780340c9fb3c5bdb6ad0f518ed98de
Zac Barreca <zacbarreca@gmail.com>
2020-02-11 14:36:34 -0500

    add personMaker

--- drills-1.js
+++ drills-1.js
@@ -21,33 +21,16 @@ function updateObject(obj, key) {
 }
 
 
-(function testUpdateObject() {
-  var oldObj = {
-    cats: 'cats',
-    dogs: 'dogs',
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+
+      return `${this.firstName} ${this.lastName}`;
+    }
   };
-  var newObj = updateObject(oldObj);
-  if (typeof newObj !== 'object') { 
-    console.error('ERROR: `createMyObject` must return an object');
-    return false
-  }
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
-    if (!(key in newObj)) {
-      console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
-      return false;
-    }
-  });
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
-    if (newObj[key] !== key) {
-      console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
-      return false;
-    }
-  });
-  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
-    console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
-    return false;
-  }
-  console.log('SUCCESS: `updateObject` works correctly!');
-  
-})();
-  
+  return person;
+}
