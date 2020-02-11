1f23283e7065ca8dcd2622addce8ee5757279a9a
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-11 14:57:32 -0500

    Added enrollInSummerSchool function

--- drills-2.js
+++ drills-2.js
@@ -8,3 +8,28 @@ function makeStudentsReport(data) {
   }
   return someArr;
 }
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  students.forEach(student => {
+    student.status = 'In Summer school';
+  });
+  return students;
+}
